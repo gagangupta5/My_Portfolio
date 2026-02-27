11f4e337281fe006f361b32ecbf540b138b529dc
Gagan Gupta <2k23.cscys2310911@gmail.com>
2026-02-27 23:27:27 +0530

    Added Home Page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -60,7 +60,7 @@ export default function Navbar(){
 
   return(
     <>
-    <nav className={`fixed top-0 left-0 w-full flex items-center justify-between px-6 py-4 z-50 transition-transform duration-300 ${visible ? "translate-y-0" : "translate-y-full"}`}>
+    <nav className={`fixed top-0 left-0 w-full flex items-center justify-between px-6 py-4 z-50 transition-transform duration-300 ${visible ? "translate-y-0" : "-translate-y-full"}`}>
       <div className="flex items-center space-x-2">
         <img src={Logo} alt="logo" className="w-8 h-8"/>
         <div className="text-2xl font-bold text-white hidden sm:block">Gagan</div>
--- src/components/ParticlesBackground.jsx
+++ src/components/ParticlesBackground.jsx
@@ -77,7 +77,7 @@ export default function ParticlesBackground() {
   return (
     <canvas 
     ref={canvasRef}
-    className="Fixed top-0 left-0 w-full h-full pointer-events-none z-0">
+    className="absolute top-0 left-0 w-full h-full pointer-events-none z-0">
 
     </canvas>
   )

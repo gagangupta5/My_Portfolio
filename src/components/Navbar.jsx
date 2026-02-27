5968c34dc408eb6980b5f0911d93c9a0b6c5d47c
Gagan Gupta <2k23.cscys2310911@gmail.com>
2026-02-27 12:48:31 +0530

    Added Navbar and OverlayMenu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import OverlayMenu from "./OverlayMenu";
 import Logo from "../assets/Logo.png";
 import {FiMenu} from "react-icons/fi";
@@ -6,10 +6,61 @@ import {FiMenu} from "react-icons/fi";
 export default function Navbar(){
   const [menuOpen, setMenuOpen] = useState(false);
   const [visible, setVisible] = useState(true);
+  const [forceVisible, setForceVisible] = useState(false);
+
+  const lastScrollY = useRef(0);
+  const timerId = useRef(null);
+
+  useEffect(() => {
+    const homeSection = document.querySelector("#home");
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if(entry.isIntersecting){
+          setForceVisible(true);
+          setVisible(true);
+        }
+        else{
+          setForceVisible(false);
+        }
+      }, {threshold: 0.1}
+    )
+    if(homeSection) observer.observe(homeSection);
+    return () => {
+      if(homeSection) observer.unobserve(homeSection);
+    }
+  }, [])
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if(forceVisible){
+        setVisible(true);
+        return;
+      }
+      const currentScrollY = window.scrollY;
+      if(currentScrollY > lastScrollY.current){
+        setVisible(false);
+      }
+      else{
+        setVisible(true);
+        if(timerId.current) clearTimeout(timerId.current);
+        timerId.current = setTimeout(() => {
+          setVisible(false);
+        }, 3000)
+      }
+        lastScrollY.current = currentScrollY;
+      }
+
+      window.addEventListener("scroll", handleScroll, {passive:true})
+
+      return () => {
+        window.removeEventListener("scroll", handleScroll)
+        if(timerId.current) clearTimeout(timerId.current);
+      }
+  }, [forceVisible])
 
   return(
     <>
-    <nav className={`fixed top-0 left-0 w-full items-center justify-between px-6 py-4 z-50 transition-transform duration-300 ${visible ? "translate-y-0" : "translate-y-full"}`}>
+    <nav className={`fixed top-0 left-0 w-full flex items-center justify-between px-6 py-4 z-50 transition-transform duration-300 ${visible ? "translate-y-0" : "translate-y-full"}`}>
       <div className="flex items-center space-x-2">
         <img src={Logo} alt="logo" className="w-8 h-8"/>
         <div className="text-2xl font-bold text-white hidden sm:block">Gagan</div>
